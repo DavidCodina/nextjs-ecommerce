29a542c68de79921f20dcef68a982a9ea3cfd606
David Codina <davidmcodina@icloud.com>
2025-04-22 13:46:38 -0600

    Random change

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -95,7 +95,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          Home (v1)
+          Home (v2)
         </Title>
 
         <ProductList data={latestProducts} title='Newest Arrivals' />
