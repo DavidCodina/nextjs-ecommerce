90fb66a83177fe55dd6dafc0fe92a5f2aad26b47
David Codina <davidmcodina@icloud.com>
2025-04-21 20:44:26 -0600

    Changed title for test

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -95,7 +95,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          Home (v1)
+          Muffy's Merch
         </Title>
 
         <ProductList data={latestProducts} title='Newest Arrivals' />
