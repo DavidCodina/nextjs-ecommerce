78314c2e25a6945c3e7810c8614c058ce8c24ccb
David Codina <davidmcodina@icloud.com>
2025-04-22 23:52:09 -0600

    Updated bash script

--- ignore-build.sh
+++ ignore-build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ "$VERCEL_GIT_PULL_REQUEST" == "false" ] && [ "$VERCEL_GIT_COMMIT_REF" != "main" ]; then
+if [ -n "$VERCEL_GIT_PULL_REQUEST" ] || [ "$VERCEL_GIT_COMMIT_REF" == "main" ]; then
+  echo "Proceeding with deployment"
+  exit 0
+else
   echo "Skipping build for non-PR and non-main branch"
   exit 1
 fi
-
-echo "Proceeding with deployment"
-exit 0
