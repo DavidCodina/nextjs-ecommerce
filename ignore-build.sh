260ada2b888684637b3afea17497501c1dfd3778
David Codina <davidmcodina@icloud.com>
2025-04-23 00:20:51 -0600

    WIP - 1745389251

--- ignore-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ "$VERCEL_GIT_COMMIT_REF" == "main" ] || [ "$VERCEL_GIT_COMMIT_REF" == "staging" ]; then
-  echo "Proceeding with deployment for main or staging"
-  echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-  exit 0
-else
-  echo "Skipping build for branch: $VERCEL_GIT_COMMIT_REF"
-  exit 1
-fi
