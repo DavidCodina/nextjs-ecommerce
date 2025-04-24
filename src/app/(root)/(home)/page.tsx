b7cfae6395dae03bd45fa70cede4657759f034eb
David Codina <davidmcodina@icloud.com>
2025-04-24 16:05:16 -0600

    WIP - 1745532316

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -95,7 +95,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          Feature D
+          Feature F
         </Title>
 
         <ProductList data={latestProducts} title='Newest Arrivals' />
