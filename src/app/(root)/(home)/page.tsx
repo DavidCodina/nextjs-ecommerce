8d033a8bd1d7681115680533de20fdcf19737566
David Codina <davidmcodina@icloud.com>
2025-04-24 16:42:31 -0600

    Updated GitHub workflow

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -95,7 +95,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          Feature F
+          Home
         </Title>
 
         <ProductList data={latestProducts} title='Newest Arrivals' />
