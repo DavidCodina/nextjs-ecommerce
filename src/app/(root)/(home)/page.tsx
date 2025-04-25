ab6863ff1ecebed3bf75217b356c2b1d0a8b106c
David Codina <davidmcodina@icloud.com>
2025-04-25 15:36:42 -0600

    F-1

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -101,7 +101,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          Home
+          Home F-1
         </Title>
 
         <pre>{JSON.stringify(result, null, 2)}</pre>
