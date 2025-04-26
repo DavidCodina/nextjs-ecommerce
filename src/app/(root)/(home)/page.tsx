cbad14ce79c731f6d825b55d69a67ac0e46f776b
David Codina <davidmcodina@icloud.com>
2025-04-25 19:41:47 -0600

    Feature 8

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -95,7 +95,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          Home
+          Home Feature 8
         </Title>
 
         <ProductList data={latestProducts} title='Newest Arrivals' />
