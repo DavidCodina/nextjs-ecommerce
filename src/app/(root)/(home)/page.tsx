1505e9123739bccc5f827cebba9d41a4ce0bcd10
David Codina <davidmcodina@icloud.com>
2025-05-18 12:08:15 -0600

    Changed title to v1

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -72,7 +72,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          PROSTORE
+          PROSTORE v1
         </Title>
 
         <ProductList data={latestProducts} title='Newest Arrivals' />
