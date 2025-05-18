95cf52ff245f18902eecd4bbb63323ef3804f61a
David Codina <davidmcodina@icloud.com>
2025-05-18 13:49:26 -0600

    Removed manual dispatch workflow

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -72,7 +72,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          PROSTORE v3
+          PROSTORE v4
         </Title>
 
         <ProductList data={latestProducts} title='Newest Arrivals' />
