0aa03b02bec2cd15bd02dccfad5ceb90efacac92
David Codina <davidmcodina@icloud.com>
2025-05-18 15:02:17 -0600

    WIP - 1747602137

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -72,7 +72,7 @@ const Home = async () => {
             textAlign: 'center'
           }}
         >
-          PROSTORE v4
+          JUNK v1
         </Title>
 
         <ProductList data={latestProducts} title='Newest Arrivals' />
